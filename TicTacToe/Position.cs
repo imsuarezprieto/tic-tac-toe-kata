0b4287540675286eb488f546aa1381784da3e52e
issuarez <issuarez@aidacanarias.com>
2021-11-12 22:20:15 +0100

    ♻️ Introduce record struct Position and Row & Column enums

---
+++ TicTacToe/Position.cs
@@ -0,0 +1,22 @@
+﻿namespace Exeal.Katas.TicTacToe
+{
+    public readonly record struct Position(
+            Row    Row,
+            Column Column );
+
+
+    public enum Row
+    {
+        Top,
+        Middle,
+        Bottom,
+    }
+
+
+    public enum Column
+    {
+        Left,
+        Middle,
+        Right,
+    }
+}
