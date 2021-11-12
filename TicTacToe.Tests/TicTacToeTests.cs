f17495a5ce73badd4d176957ef0f183e1f915e9d
issuarez <issuarez@aidacanarias.com>
2021-11-12 15:46:00 +0100

    ✨ Game with an empty board

--- TicTacToe.Tests/TicTacToeTests.cs
+++ TicTacToe.Tests/TicTacToeTests.cs
@@ -1,11 +1,17 @@
 namespace Exeal.Katas.TicTacToe.Tests
 {
+    using System.Collections;
     using Xunit;
 
 
     public sealed class UnitTest1
     {
         [Fact]
-        public void Test1 () { }
+        public void Game_starts_with_an_empty_board ()
+        {
+            IEnumerable newGameBoard = new Game().Board;
+
+            Assert.Empty( newGameBoard );
+        }
     }
 }
---
+++ TicTacToe/Game.cs
@@ -0,0 +1,10 @@
+﻿namespace Exeal.Katas.TicTacToe
+{
+    using System.Collections;
+
+
+    public sealed class Game
+    {
+        public IEnumerable Board { get; set; } = new ArrayList();
+    }
+}
