9b793e0168320a4dec10599abdc79ea80542d849
issuarez <issuarez@aidacanarias.com>
2021-11-12 15:32:06 +0100

    🎉 Initial black solution with library and tests

---
+++ TicTacToe.Tests/TicTacToeTests.cs
@@ -0,0 +1,11 @@
+namespace Exeal.Katas.TicTacToe.Tests
+{
+    using Xunit;
+
+
+    public sealed class UnitTest1
+    {
+        [Fact]
+        public void Test1 () { }
+    }
+}
---
+++ TicTacToe/Class1.cs
@@ -0,0 +1,1 @@
+﻿namespace Exeal.Katas.TicTacToe { }
