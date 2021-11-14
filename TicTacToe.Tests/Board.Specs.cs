87d9c06d88e65f86fda76e8eef5b3652d48da735
issuarez <issuarez@aidacanarias.com>
2021-11-14 11:47:26 +0100

    ✅ Empty board specification avoids just for testing method

--- TicTacToe.Tests/Board.Specs.cs
+++ TicTacToe.Tests/Board.Specs.cs
@@ -3,6 +3,8 @@
 
 namespace Exeal.Katas.TicTacToe.Tests
 {
+	using System;
+	using System.Collections.Generic;
 	using FluentAssertions;
 	using Xunit;
 
@@ -10,13 +12,31 @@ namespace Exeal.Katas.TicTacToe.Tests
 	[Collection( "Rule 0" )]
 	public sealed class Starts_empty_Board
 	{
-		[Fact]
-		public void Game_starts_with_an_empty_board ()
+		public static IEnumerable<Object[]> EveryPosition
+			=> new List<Object[]> {
+					new Object[] { Position.Top.Left },
+					new Object[] { Position.Top.Middle },
+					new Object[] { Position.Top.Right },
+					new Object[] { Position.Middle.Left },
+					new Object[] { Position.Center },
+					new Object[] { Position.Middle.Right },
+					new Object[] { Position.Bottom.Left },
+					new Object[] { Position.Bottom.Middle },
+					new Object[] { Position.Bottom.Right },
+			};
+
+		[Theory]
+		[MemberData( nameof(Starts_empty_Board.EveryPosition) )]
+		public void Starting_players_can_mark_any_position (
+				IPosition position )
 		{
 			var newGame = new Game();
 
-			newGame.HasEmptyBoard()
-					.Should().BeTrue();
+			Action player_marks_position_on_board = () =>
+					newGame.Play( position );
+
+			player_marks_position_on_board
+					.Should().NotThrow();
 		}
 	}
 
