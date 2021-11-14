b4831ba07509f1dee12bbb4795bc6815a24d6c7e
issuarez <issuarez@aidacanarias.com>
2021-11-14 16:02:27 +0100

    🎨 'Player' layout

--- TicTacToe/Player.cs
+++ TicTacToe/Player.cs
@@ -2,19 +2,19 @@
 {
 	public sealed class Player
 	{
+		public static Player X { get; } = new()  { Mark = Mark.X };
+		public static Player O { get; } = new()  { Mark = Mark.O };
+
 		static Player ()
 		{
 			Player.X.NextPlayer = Player.O;
 			Player.O.NextPlayer = Player.X;
 		}
 
-		private Player () { }
-
-		public static Player X { get; } = new()  { Mark = Mark.X };
-		public static Player O { get; } = new()  { Mark = Mark.O };
-
 		internal Mark   Mark       { get; private init; }
 		private  Player NextPlayer { get; set; }
+
+		private Player () { }
 
 		public static Player operator ++ (
 				Player player )
