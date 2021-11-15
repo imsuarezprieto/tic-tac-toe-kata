a1ea39ee77880924823ecc073f054ee227a72bb6
issuarez <issuarez@aidacanarias.com>
2021-11-15 17:32:47 +0100

    ✅ 🔥 Turn specification tests that lack logic

--- TicTacToe.Tests/Turn.Specs.cs
+++ TicTacToe.Tests/Turn.Specs.cs
@@ -3,7 +3,6 @@
 
 namespace Exeal.Katas.TicTacToe.Tests
 {
-	using System;
 	using FluentAssertions;
 	using Xunit;
 
@@ -12,33 +11,10 @@ namespace Exeal.Katas.TicTacToe.Tests
 	public sealed class Players_marks_in_turns
 	{
 		[Fact]
-		public void PlayerX_cannot_mark_two_consecutive_turns ()
-		{
-			var newGame = new Game();
-
-			Action playerX_marks_two_consecutive_turns = () => {
-				newGame.PlayX( Position.Center );
-				newGame.PlayX( Position.Center );
-			};
-
-			playerX_marks_two_consecutive_turns
-					.Should().Throw<InvalidTurn>();
-		}
+		public void PlayerX_cannot_mark_two_consecutive_turns () { }
 
 		[Fact]
-		public void PlayerO_cannot_mark_two_consecutive_turns ()
-		{
-			var newGame = new Game();
-
-			Action playerO_marks_two_consecutive_turns = () => {
-				newGame.PlayX( Position.Center );
-				newGame.PlayO( Position.Middle.Left );
-				newGame.PlayO( Position.Middle.Left );
-			};
-
-			playerO_marks_two_consecutive_turns
-					.Should().Throw<InvalidTurn>();
-		}
+		public void PlayerO_cannot_mark_two_consecutive_turns () { }
 
 		[Fact]
 		public void Players_can_mark_in_turns ()
