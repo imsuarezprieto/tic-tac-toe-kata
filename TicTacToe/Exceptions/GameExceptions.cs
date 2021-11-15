6291920e55b2215552bd224537affa47b8c381e9
issuarez <issuarez@aidacanarias.com>
2021-11-15 19:11:45 +0100

    🔥 Remove unused exception

--- TicTacToe/Exceptions/GameExceptions.cs
+++ TicTacToe/Exceptions/GameExceptions.cs
@@ -1,7 +1,8 @@
 ﻿namespace Exeal.Katas.TicTacToe.Exceptions
 {
     using System;
-    public sealed class InvalidTurn : Exception { }
+
+
     public sealed class AlreadyMarkedPosition : Exception { }
-    public sealed class EndedGame : Exception { }
+    public sealed class EndedGame             : Exception { }
 }
